3b291ea52821f121bb5c2d6284410cc883af1d30
BhardwajPriyanshu <153509529+BhardwajPriyanshu@users.noreply.github.com>
2023-12-11 14:14:23 +0000

    doing

---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+let accountId = 202001;
+// const accountEmail = "bhardwaj@mail.com";
+// var accountPassword = "98997283";
+// accountCity = "jaipur";
+console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
